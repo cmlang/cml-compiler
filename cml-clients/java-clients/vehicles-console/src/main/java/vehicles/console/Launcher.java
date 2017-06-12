829fb215d89f1e14dc3e06614ae73a0cd6a0059c
Quenio dos Santos <quenio@me.com>
2017-06-12 14:27:21 -0300

    Verifying association getters.

--- cml-clients/java-clients/vehicles-console/src/main/java/vehicles/console/Launcher.java
+++ cml-clients/java-clients/vehicles-console/src/main/java/vehicles/console/Launcher.java
@@ -16,10 +16,29 @@ public class Launcher
 
         final Organization organization = new Organization("Walt Disney", emptyList(), emptyList());
         final Employee donald = new Employee("Donald Duck", organization);
-        final Vehicle vehicle = new Vehicle("DUCK", donald, organization);
+        final Employee mickey = new Employee("Mickey Mouse", organization);
+        final Vehicle duck = new Vehicle("DUCK", donald, organization);
+        final Vehicle mouse = new Vehicle("MOUSE", mickey, organization);
 
         System.out.println(organization);
+        System.out.println("- Employees: " + organization.getEmployees());
+        System.out.println("- Fleet: " + organization.getFleet());
+        System.out.println();
+
         System.out.println(donald);
-        System.out.println(vehicle);
+        System.out.println("- Employer: " + donald.getEmployer());
+        System.out.println();
+
+        System.out.println(mickey);
+        System.out.println("- Employer: " + mickey.getEmployer());
+        System.out.println();
+
+        System.out.println(duck);
+        System.out.println("- Owner: " + duck.getOwner());
+        System.out.println();
+
+        System.out.println(mouse);
+        System.out.println("- Owner: " + mouse.getOwner());
+        System.out.println();
     }
 }
--- cml-clients/py-clients/vehicles_console.py
+++ cml-clients/py-clients/vehicles_console.py
@@ -7,8 +7,27 @@ print()
 
 organization = Organization("Walt Disney", [], [])
 donald = Employee("Donald Duck", organization)
-vehicle = Vehicle("DUCK", donald, organization)
+mickey = Employee("Mickey Mouse", organization)
+duck = Vehicle("DUCK", donald, organization)
+mouse = Vehicle("MOUSE", mickey, organization)
 
 print(organization)
+print("- Employees: [%s]" % ', '.join(map(str, organization.employees)))
+print("- Fleet: [%s]" % ', '.join(map(str, organization.fleet)))
+print()
+
 print(donald)
-print(vehicle)
+print("- Employer: %s" % donald.employer)
+print()
+
+print(mickey)
+print("- Employer: %s" % mickey.employer)
+print()
+
+print(duck)
+print("- Owner: %s" % duck.owner)
+print()
+
+print(mouse)
+print("- Owner: %s" % mouse.owner)
+print()
