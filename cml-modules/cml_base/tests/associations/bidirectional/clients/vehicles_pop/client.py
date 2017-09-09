5cfce49f09695ca5a22a223ac6231cd8ce5c29a0
Quenio dos Santos <quenio@me.com>
2017-09-09 13:14:09 -0300

    Modified client for bidirectional associations to include multiple vehicles and drivers (pop)

--- cml-modules/cml_base/tests/associations/bidirectional/clients/vehicles_pop/client.py
+++ cml-modules/cml_base/tests/associations/bidirectional/clients/vehicles_pop/client.py
@@ -3,14 +3,30 @@ from cml_associations_bidirectional_vehicles_pop import *
 
 print("Bidirectional Associations (pop)\n")
 
-organization = Organization("Acme", [], [])
-employee = Employee("John", organization)
-vehicle = Vehicle("ABC12345", employee, organization)
+organization = Organization("Walt Disney", [], [])
+donald = Employee("Donald Duck", organization)
+mickey = Employee("Mickey Mouse", organization)
+duck = Vehicle("DUCK", donald, organization)
+mouse = Vehicle("MOUSE", mickey, organization)
 
-print("Organization: %s" % organization)
-print("Organization's Employees: %s" % ','.join(map(str, organization.employees)))
-print("Employee: %s" % employee)
-print("Employee's Employer: %s" % employee.employer)
-print("Vehicle: %s" % vehicle)
-print("Vehicle's Driver: %s" % vehicle.driver)
-print("Vehicle's Owner: %s" % vehicle.owner)
+print(organization)
+print("- Employees: %s" % ', '.join(map(str, organization.employees)))
+print("- Fleet: %s" % ', '.join(map(str, organization.fleet)))
+print()
+
+print(donald)
+print("- Employer: %s" % donald.employer)
+print()
+
+print(mickey)
+print("- Employer: %s" % mickey.employer)
+print()
+
+print(duck)
+print("- Owner: %s" % duck.owner)
+print("- Driver: %s" % duck.driver)
+print()
+
+print(mouse)
+print("- Owner: %s" % mouse.owner)
+print("- Driver: %s" % mouse.driver)
