2c396deba1991193e9bb13f2c423c653585a49e5
Quenio dos Santos <quenio@me.com>
2017-09-09 12:06:40 -0300

    Client to verify bidirectional associations with cmlc_java task.

---
+++ cml-modules/cml_base/tests/associations/bidirectional/clients/vehicles_cmlc_java/src/main/java/vehicles/client/Launcher.java
@@ -0,0 +1,26 @@
+package vehicles.client;
+
+import vehicles.cmlc.Employee;
+import vehicles.cmlc.Organization;
+import vehicles.cmlc.Vehicle;
+
+import static java.util.Collections.emptyList;
+
+public class Launcher
+{
+    public static void main(final String[] args)
+    {
+        System.out.println("Bidirectional Associations (cmlc_java)\n");
+
+        final Organization organization = Organization.createOrganization("Acme", emptyList(), emptyList());
+        final Employee employee = Employee.createEmployee("John", organization);
+        final Vehicle vehicle = Vehicle.createVehicle("ABC12345", employee, organization);
+
+        System.out.println("Employee: " + employee);
+        System.out.println("Organization: " + organization);
+        System.out.println("Organization's Employees: " + organization.getEmployees());
+        System.out.println("Vehicle: " + vehicle);
+        System.out.println("Vehicle's Driver: " + vehicle.getDriver());
+        System.out.println("Vehicle's Owner: " + vehicle.getOwner());
+    }
+}
