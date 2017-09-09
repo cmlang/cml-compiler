8c3751bc9513da3a477b5ac965b9b773d6670dd2
Quenio dos Santos <quenio@me.com>
2017-09-09 12:13:44 -0300

    Client to verify bidirectional associations with poj task.

---
+++ cml-modules/cml_base/tests/associations/bidirectional/clients/vehicles_poj/src/main/java/vehicles/client/Launcher.java
@@ -0,0 +1,26 @@
+package vehicles.client;
+
+import vehicles.poj.Employee;
+import vehicles.poj.Organization;
+import vehicles.poj.Vehicle;
+
+import static java.util.Collections.emptyList;
+
+public class Launcher
+{
+    public static void main(final String[] args)
+    {
+        System.out.println("Bidirectional Associations (poj)\n");
+
+        final Organization organization = new Organization("Acme", emptyList(), emptyList());
+        final Employee employee = new Employee("John", organization);
+        final Vehicle vehicle = new Vehicle("ABC12345", employee, organization);
+
+        System.out.println("Employee: " + employee);
+        System.out.println("Organization: " + organization);
+        System.out.println("Organization's Employees: " + organization.getEmployees());
+        System.out.println("Vehicle: " + vehicle);
+        System.out.println("Vehicle's Driver: " + vehicle.getDriver());
+        System.out.println("Vehicle's Owner: " + vehicle.getOwner());
+    }
+}
