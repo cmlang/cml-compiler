4c35e0afeaa3a584b8d6aedea0a1b7a6e86a76be
Quenio dos Santos <quenio@me.com>
2017-09-09 13:09:37 -0300

    Modified client for bidirectional associations to include multiple vehicles and drivers (cmlc_py)

--- cml-modules/cml_base/tests/associations/bidirectional/clients/vehicles_cmlc_py/client.py
+++ cml-modules/cml_base/tests/associations/bidirectional/clients/vehicles_cmlc_py/client.py
@@ -3,14 +3,31 @@ from cml_associations_bidirectional_vehicles_cmlc import *
 
 print("Bidirectional Associations (cmlc_py)\n")
 
-organization = Organization.create_organization("Acme", [], [])
-employee = Employee.create_employee("John", organization)
-vehicle = Vehicle.create_vehicle("ABC12345", employee, organization)
+organization = Organization.create_organization("Walt Disney", [], [])
+donald = Employee.create_employee("Donald Duck", organization)
+mickey = Employee.create_employee("Mickey Mouse", organization)
+duck = Vehicle.create_vehicle("DUCK", donald, organization)
+mouse = Vehicle.create_vehicle("MOUSE", mickey, organization)
 
-print("Organization: %s" % organization)
-print("Organization's Employees: %s" % ','.join(map(str, organization.employees)))
-print("Employee: %s" % employee)
-print("Employee's Employer: %s" % employee.employer)
-print("Vehicle: %s" % vehicle)
-print("Vehicle's Driver: %s" % vehicle.driver)
-print("Vehicle's Owner: %s" % vehicle.owner)
+print(organization)
+print("- Employees: %s" % ', '.join(map(str, organization.employees)))
+print("- Fleet: %s" % ', '.join(map(str, organization.fleet)))
+print()
+
+print(donald)
+print("- Employer: %s" % donald.employer)
+print()
+
+print(mickey)
+print("- Employer: %s" % mickey.employer)
+print()
+
+print(duck)
+print("- Owner: %s" % duck.owner)
+print("- Driver: %s" % duck.driver)
+print()
+
+print(mouse)
+print("- Owner: %s" % mouse.owner)
+print("- Driver: %s" % mouse.driver)
+
