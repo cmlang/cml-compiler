5fa6b6814713caf8d7de1cac7a89395f910788cb
Quenio dos Santos <quenio@me.com>
2017-09-10 23:52:39 -0300

    Removed tests that have been migrated to cml_base.

--- cml-acceptance/src/test/java/cml/acceptance/AcceptanceTest.java
+++ cml-acceptance/src/test/java/cml/acceptance/AcceptanceTest.java
@@ -66,10 +66,7 @@ public class AcceptanceTest
         new SuccessCase("expressions", "expressions-console", "expressions_poj", JAVA),
         new SuccessCase("expressions", "expressions_console", "expressions_pop", PYTHON),
         new SuccessCase("livir_books", "livir-console", "livir_poj", JAVA),
-        new SuccessCase("livir_books", "livir_console", "livir_pop", PYTHON),
-        new SuccessCase("shapes", "shapes-console", "shapes_java", JAVA),
-        new SuccessCase("shapes", "shapes_cmlc_console", "shapes_cmlc_py", PYTHON, "shapes_cmlc")
-//        new SuccessCase("shapes", "shapes_pop_console", "shapes_pop_py", PYTHON, "shapes_pop"), // failing
+        new SuccessCase("livir_books", "livir_console", "livir_pop", PYTHON)
     };
 
     @DataPoints("validation-modules")
