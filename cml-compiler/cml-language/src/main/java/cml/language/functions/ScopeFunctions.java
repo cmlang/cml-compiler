e0c4faf4f2980cc019b7b246d05ec28964e4ec39
Quenio dos Santos <quenio@me.com>
2017-09-23 09:51:49 -0300

    Finding properties in ancestors.

--- cml-compiler/cml-language/src/main/java/cml/language/functions/ScopeFunctions.java
+++ cml-compiler/cml-language/src/main/java/cml/language/functions/ScopeFunctions.java
@@ -2,6 +2,7 @@ package cml.language.functions;
 
 import cml.language.expressions.LambdaScope;
 import cml.language.expressions.Path;
+import cml.language.features.Concept;
 import cml.language.generated.NamedElement;
 import cml.language.generated.Scope;
 import cml.language.types.NamedType;
@@ -62,16 +63,6 @@ public class ScopeFunctions
         return empty();
     }
 
-    public static Optional<Type> typeOfElementNamed(String name, Scope scope)
-    {
-        final Optional<Type> memberType = typeOfMemberNamed(name, scope);
-        if (memberType.isPresent()) return memberType;
-
-        final Optional<TypedElement> typedElement = elementNamed(name, scope, TypedElement.class);
-
-        return typedElement.map(TypedElement::getType);
-    }
-
     public static Optional<Scope> scopeOfType(Type type, Scope scope)
     {
         if (type instanceof NamedType)
@@ -90,10 +81,7 @@ public class ScopeFunctions
             final LambdaScope lambdaScope = (LambdaScope) scope;
             final Optional<Type> type = ofNullable(lambdaScope.getParameters().get(name));
 
-            if (type.isPresent())
-            {
-                return type;
-            }
+            if (type.isPresent()) return type;
         }
         else if (scope instanceof Path)
         {
@@ -101,15 +89,27 @@ public class ScopeFunctions
             final Optional<Scope> typeScope = scopeOfType(path.getType(), scope);
             final Optional<Type> type = typeScope.flatMap(s -> typeOfVariableNamed(name, s));
 
-            if (type.isPresent())
+            if (type.isPresent()) return type;
+        }
+        else if (scope instanceof Concept)
+        {
+            final Optional<Type> memberType = typeOfMemberNamed(name, scope);
+            if (memberType.isPresent()) return memberType;
+
+            final Concept concept = (Concept) scope;
+
+            for (Concept ancestor: concept.getDirectAncestors())
             {
-                return type;
+                final Optional<Type> type = typeOfVariableNamed(name, ancestor);
+
+                if (type.isPresent()) return type;
             }
         }
-
-        final Optional<Type> type = typeOfElementNamed(name, scope);
-
-        if (type.isPresent()) return type;
+        else
+        {
+            final Optional<Type> memberType = typeOfMemberNamed(name, scope);
+            if (memberType.isPresent()) return memberType;
+        }
 
         return scope.getParent().flatMap(s -> typeOfVariableNamed(name, s));
     }
